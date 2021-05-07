e9ddd74196b998e8529e13f7e91df951f55b6994
Ahmet Alp Balkan <ahmetb@google.com>
2021-05-07 15:24:03 -0700

    add terraform specs
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,20 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/google" {
+  version = "3.66.1"
+  hashes = [
+    "h1:oyVU+NXuJovmCHpyj9vvuUnDxgGFNN+HyCgbIBoEcug=",
+    "zh:18c6e686439f2b8ab054cc22ddd42737f0e9cc193798e7bdbf230ce8a64c0e57",
+    "zh:400fad5207b606972b91b0f6cb599b79e2f473435760236780001eb11f4f88a5",
+    "zh:46e4fb036ab56067ca4c121a89bde23ddd4c50e3e696d2d826efa8583d7c8845",
+    "zh:48438bf8be72adec0bc3ee23eca05db2ab772f4a7e31d22cccf217e0ddb7b316",
+    "zh:4b2d88ff13624b7a9e5bb1b84e290cb6cc64090e0d29e54b536a0d931ebdaa56",
+    "zh:5485b05bc3d03319f4d11fa53821f446ba3d26e500e616fc4bd4248de41cb637",
+    "zh:a45ef1cc10a7eac6cffe8f7c84e07f05e2db6c56645141a58c8ebb3cbe25a5c0",
+    "zh:bfd17635f3a9e35362d112483d226d9e4dcd94556af5b06b1127a843adc132f9",
+    "zh:d03ed0ece4438b8377ba5442f7f8d09e1aae0b04d47b39ba56e10379ed562d16",
+    "zh:d72a78c99806cdf305c39c684882200b1e4cc64a27c7e11f8383545537102977",
+    "zh:e7ed250128ce98beea9c9526543dc109f90d085e0f581172f94815496d6bdebd",
+  ]
+}
---
+++ terraform/db.tf
@@ -0,0 +1,51 @@
+
+variable "project" {
+  default = "grpcoin"
+}
+
+variable "billing_account" {
+  default = "005E60-364ED9-72CEA0"
+}
+
+provider "google" {
+  project = var.project
+}
+
+resource "google_project" "default" {
+  name            = var.project
+  project_id      = var.project
+  billing_account = var.billing_account
+}
+
+resource "google_project_service" "compute" {
+  project = var.project
+  service = "compute.googleapis.com"
+}
+
+resource "google_project_service" "firestore" {
+  project = var.project
+  service = "firestore.googleapis.com"
+}
+
+# TODO enable later when setting up a clean project
+resource "google_app_engine_application" "app" {
+  project       = var.project
+  location_id   = "us-west2"
+  database_type = "CLOUD_FIRESTORE"
+}
+
+resource "google_compute_network" "vpc" {
+  project = var.project
+  name    = "vpc"
+  depends_on = [
+    google_project_service.compute
+  ]
+}
+
+resource "google_redis_instance" "cache" {
+  project            = var.project
+  name               = "cache"
+  region             = "us-west2"
+  memory_size_gb     = 1
+  authorized_network = google_compute_network.vpc.name
+}
