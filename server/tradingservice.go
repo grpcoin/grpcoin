c009ec80240a1d22112ed2831cd8b25f86badd33
Ahmet Alp Balkan <ahmetb@google.com>
2021-05-13 12:57:43 -0700

    trade validation tests and quote provider
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- server/tradingservice.go
+++ server/tradingservice.go
@@ -16,16 +16,78 @@ package main
 
 import (
 	"context"
+	"errors"
+	"log"
+	"sync"
+	"time"
 
 	"github.com/grpcoin/grpcoin/api/grpcoin"
+	"github.com/grpcoin/grpcoin/gdax"
 	"github.com/grpcoin/grpcoin/server/userdb"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
+type QuoteProvider interface {
+	// GetQuote provides real-time quote for ticker.
+	// Can quit early if ctx is cancelled.
+	GetQuote(ctx context.Context, ticker string) (*grpcoin.Amount, error)
+}
+
+type coinbaseQuoteProvider struct {
+	ticker string
+
+	lock        sync.RWMutex
+	lastQuote   *grpcoin.Amount
+	lastUpdated time.Time
+}
+
+const staleQuotePeriod = time.Second
+
+func (cb *coinbaseQuoteProvider) GetQuote(ctx context.Context, ticker string) (*grpcoin.Amount, error) {
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+			cb.lock.RLock()
+			if time.Since(cb.lastUpdated) > staleQuotePeriod {
+				cb.lock.RUnlock()
+				break
+			}
+			q := cb.lastQuote
+			cb.lock.RUnlock()
+			return q, nil
+		}
+		time.Sleep(time.Millisecond * 10) // TODO not so great but prevents 100% cpu
+	}
+}
+
+// sync continually maintains a channel to Coinbase realtime prices.
+// meant to be invoked in a goroutine
+func (cb *coinbaseQuoteProvider) sync() {
+	for {
+		ch, err := gdax.StartWatch(context.Background(), cb.ticker)
+		if err != nil {
+			// TODO plumb logger here
+			log.Printf("warning: failed to connected to coinbase: %v", err)
+			// TODO consider adding sleep/backoff to prevent DoSing coinbase API
+			continue
+		}
+		for m := range ch {
+			cb.lock.Lock()
+			cb.lastUpdated = time.Now()
+			cb.lastQuote = m.Price
+			cb.lock.Unlock()
+		}
+		log.Printf("warn: coinbase channel terminated, reopening")
+	}
+}
+
 type tradingService struct {
 	udb *userdb.UserDB
+	tp  QuoteProvider
 
 	grpcoin.UnimplementedPaperTradeServer
 }
@@ -53,27 +115,50 @@ func (t *tradingService) Trade(ctx context.Context, req *grpcoin.TradeRequest) (
 	if !ok {
 		return nil, status.Error(codes.Internal, "could not find user record in request context")
 	}
-	if req.Action != grpcoin.TradeAction_BUY && req.Action != grpcoin.TradeAction_SELL {
-		return nil, status.Errorf(codes.InvalidArgument, "invalid trade action: %s", req.GetAction().Enum().String())
+	if err := validateTradeRequest(req); err != nil {
+		return nil, err
 	}
-	if req.Quantity.Units < 0 {
-		return nil, status.Errorf(codes.InvalidArgument, "negative quantity units (%d)", req.GetQuantity().GetUnits())
+
+	// get a real-time market quote
+	quoteDeadline := time.Second
+	quoteCtx, cancel := context.WithTimeout(ctx, quoteDeadline)
+	defer cancel()
+	quote, err := t.tp.GetQuote(quoteCtx, req.GetTicker().Ticker)
+	if errors.Is(err, context.DeadlineExceeded) {
+		return nil, status.Errorf(codes.Unavailable, "could not get real-time market quote for %s in %v",
+			req.GetTicker().GetTicker(), quoteDeadline)
+	} else if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to retrieve a quote: %v", err)
 	}
-	if req.Quantity.Nanos < 0 {
-		return nil, status.Errorf(codes.InvalidArgument, "negative quantity nanos (%d)", req.GetQuantity().GetNanos())
-	}
-	if req.GetTicker().GetTicker() == "" {
-		return nil, status.Error(codes.InvalidArgument, "ticker not specified")
-	}
-	if req.GetTicker().GetTicker() != "BTC" {
-		return nil, status.Errorf(codes.InvalidArgument, "ticker '%s' not specified, only 'BTC' is supported", req.Ticker.Ticker)
-	}
-	_ = user
+
 	// TODO execute trade on UserDB.
+	_ = user
 	return &grpcoin.TradeResponse{
 		T:             timestamppb.Now(), // TODO read from tx
 		Action:        req.Action,
-		ExecutedPrice: &grpcoin.Amount{}, // TODO plumb ticker service here
+		ExecutedPrice: quote,
 		Quantity:      req.Quantity,
 	}, nil
+}
+
+func validateTradeRequest(req *grpcoin.TradeRequest) error {
+	if req.Action != grpcoin.TradeAction_BUY && req.Action != grpcoin.TradeAction_SELL {
+		return status.Errorf(codes.InvalidArgument, "invalid trade action: %s", req.GetAction().Enum().String())
+	}
+	if req.GetQuantity() == nil || (req.GetQuantity().Nanos == 0 && req.GetQuantity().Units == 0) {
+		return status.Error(codes.InvalidArgument, "quantity not specified")
+	}
+	if req.GetQuantity().Units < 0 {
+		return status.Errorf(codes.InvalidArgument, "negative quantity units (%d)", req.GetQuantity().GetUnits())
+	}
+	if req.GetQuantity().Nanos < 0 {
+		return status.Errorf(codes.InvalidArgument, "negative quantity nanos (%d)", req.GetQuantity().GetNanos())
+	}
+	if req.GetTicker().GetTicker() == "" {
+		return status.Error(codes.InvalidArgument, "ticker not specified")
+	}
+	if req.GetTicker().GetTicker() != "BTC" {
+		return status.Errorf(codes.InvalidArgument, "ticker '%s' not specified, only 'BTC' is supported", req.Ticker.Ticker)
+	}
+	return nil
 }
--- server/tradingservice_test.go
+++ server/tradingservice_test.go
@@ -11,14 +11,34 @@ import (
 	"github.com/grpcoin/grpcoin/server/auth"
 	"github.com/grpcoin/grpcoin/server/firestoretestutil"
 	"github.com/grpcoin/grpcoin/server/userdb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+var _ QuoteProvider = &coinbaseQuoteProvider{}
+
+type mockQuoteProvider struct {
+	a   *grpcoin.Amount
+	err error
+}
+
+func (m *mockQuoteProvider) GetQuote(ctx context.Context, ticker string) (*grpcoin.Amount, error) {
+	return m.a, m.err
+}
 
 func TestPortfolio(t *testing.T) {
 	fs := firestoretestutil.StartEmulator(t, context.TODO())
 	udb := &userdb.UserDB{DB: fs}
 
-	ctx := auth.WithUser(context.Background(), &github.GitHubUser{ID: 1, Username: "abc"})
+	au := &github.GitHubUser{ID: 1, Username: "abc"}
+	user, err := udb.EnsureAccountExists(context.TODO(), au)
+	if err != nil {
+		t.Fatal(err)
+	}
 	pt := &tradingService{udb: udb}
+
+	ctx := auth.WithUser(context.Background(), au)
+	ctx = userdb.WithUserRecord(ctx, user)
 
 	resp, err := pt.Portfolio(ctx, &grpcoin.PortfolioRequest{})
 	if err != nil {
@@ -43,5 +63,110 @@ func TestPortfolio(t *testing.T) {
 	))
 	if diff != "" {
 		t.Fatal(diff)
+	}
+}
+
+func TestTradeQuotePrices(t *testing.T) {
+	fs := firestoretestutil.StartEmulator(t, context.TODO())
+	udb := &userdb.UserDB{DB: fs}
+
+	faultyQuote := &mockQuoteProvider{err: context.DeadlineExceeded}
+	pt := &tradingService{udb: udb, tp: faultyQuote}
+	au := &github.GitHubUser{ID: 1, Username: "abc"}
+	user, err := udb.EnsureAccountExists(context.TODO(), au)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := auth.WithUser(context.Background(), au)
+	ctx = userdb.WithUserRecord(ctx, user)
+
+	_, err = pt.Trade(context.TODO(), &grpcoin.TradeRequest{})
+	if status.Code(err) != codes.Internal {
+		t.Fatalf("expected internal error when unauthenticated: %v", err)
+	}
+
+	_, err = pt.Trade(ctx, &grpcoin.TradeRequest{
+		Action:   grpcoin.TradeAction_BUY,
+		Ticker:   &grpcoin.TradeRequest_Ticker{Ticker: "BTC"},
+		Quantity: &grpcoin.Amount{Units: 1},
+	})
+	if status.Code(err) != codes.Unavailable {
+		t.Fatalf("expected unavailable error when quote cannot be recvd: %v", err)
+	}
+
+	pt.tp = &mockQuoteProvider{a: &grpcoin.Amount{Units: 50_000}}
+	resp, err := pt.Trade(ctx, &grpcoin.TradeRequest{
+		Action:   grpcoin.TradeAction_BUY,
+		Ticker:   &grpcoin.TradeRequest_Ticker{Ticker: "BTC"},
+		Quantity: &grpcoin.Amount{Units: 1},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if diff := cmp.Diff(resp.GetExecutedPrice(), &grpcoin.Amount{Units: 50_000},
+		cmpopts.IgnoreUnexported(grpcoin.Amount{})); diff != "" {
+		t.Fatal(diff)
+	}
+}
+
+func Test_validateTradeRequest(t *testing.T) {
+	tests := []struct {
+		name string
+		req  *grpcoin.TradeRequest
+		code codes.Code
+	}{
+		{
+			name: "empty",
+			req:  &grpcoin.TradeRequest{},
+			code: codes.InvalidArgument,
+		},
+		{
+			name: "empty ticker",
+			req:  &grpcoin.TradeRequest{Action: grpcoin.TradeAction_SELL},
+			code: codes.InvalidArgument,
+		},
+		{
+			name: "wrong ticker",
+			req: &grpcoin.TradeRequest{Action: grpcoin.TradeAction_SELL,
+				Ticker: &grpcoin.TradeRequest_Ticker{Ticker: "XXX"}},
+			code: codes.InvalidArgument,
+		},
+		{
+			name: "missing quantity",
+			req: &grpcoin.TradeRequest{Action: grpcoin.TradeAction_SELL,
+				Ticker: &grpcoin.TradeRequest_Ticker{Ticker: "BTC"}},
+			code: codes.InvalidArgument,
+		},
+		{
+			name: "zero quantity",
+			req: &grpcoin.TradeRequest{Action: grpcoin.TradeAction_SELL,
+				Ticker:   &grpcoin.TradeRequest_Ticker{Ticker: "BTC"},
+				Quantity: &grpcoin.Amount{}},
+			code: codes.InvalidArgument,
+		},
+		{
+			name: "negative unit",
+			req: &grpcoin.TradeRequest{Action: grpcoin.TradeAction_SELL,
+				Ticker:   &grpcoin.TradeRequest_Ticker{Ticker: "BTC"},
+				Quantity: &grpcoin.Amount{Units: -1},
+			},
+			code: codes.InvalidArgument,
+		},
+		{
+			name: "negative nanos",
+			req: &grpcoin.TradeRequest{Action: grpcoin.TradeAction_SELL,
+				Ticker:   &grpcoin.TradeRequest_Ticker{Ticker: "BTC"},
+				Quantity: &grpcoin.Amount{Nanos: -1},
+			},
+			code: codes.InvalidArgument,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := validateTradeRequest(tt.req); status.Code(err) != tt.code {
+				t.Errorf("validateTradeRequest() error = %v, wantErr %s", err, tt.code)
+			}
+		})
 	}
 }
